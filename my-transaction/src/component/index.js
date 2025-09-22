6365e2686cf1e9bbe1344752bf5fddb226cbf0a9
fitriabudipertiwi28-web <fitriabudipertiwi28@gmail.com>
2025-09-22 15:50:45 +0700

    update

---
+++ my-transaction/src/component/index.js
@@ -0,0 +1,7 @@
+import Hasil from './Hasil'
+import ListCategory from './ListCategory'
+import NavbarComp from './navbar'
+import Menus from './Menus'
+import { Navbar } from 'react-bootstrap'
+
+export default {Hasil,ListCategory,NavbarComp, Menus}
